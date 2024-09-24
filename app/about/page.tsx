9b2af52cc30f2f45c84acf97f59c82fcf31767ff
sbcombo <schoi0115@gmail.com>
2024-09-24 15:47:19 -0400

    loading page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from "next/link"
+
+export default function About() {
+    return (
+        <>
+            <div>About</div>
+            <Link href="/">Link to Home</Link>
+        </>
+
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import './globals.css'
 
 export default function RootLayout({
   children,
@@ -12,7 +11,14 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <nav>
+          <h1>
+            My navebar
+          </h1>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
